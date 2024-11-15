1cd4b695c93ba5cec49217ee3c64d8d1d56b42c1
truonthu <thuy.truong@student.uni-augsburg.de>
2024-11-15 16:58:49 +0100

    renamed bachelorthesis folder to AutomatizedEigensolver

--- run_eigensolver.sh
+++ run_eigensolver.sh
@@ -2,6 +2,6 @@
 rm -f *.dat *.tex 
 cd ../build/ && make -j8 && cd -
 #../build/src/examples/1dharmonic
-../build/src/bachelorthesis/eigensolver
+../build/src/AutomatizedEigensolver/eigensolver
 ./plot_1D.py $(ls Psi_*.dat) potential.dat
 #./plot_1D_2.py $(ls Psi*.dat) potential.dat 
