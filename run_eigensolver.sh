d4d5b271f2743d0419543cbebff10dcb5572269a
truonthu <thuy.truong@student.uni-augsburg.de>
2024-11-22 15:25:36 +0100

    cleaned code and added comments

--- run_eigensolver.sh
+++ run_eigensolver.sh
@@ -1,7 +1,33 @@
+# -----------------------------------------------------------------------------------------
+# This script is used to build and run the Eigensolver project and plot the results. 
+# Before running this script, make sure that the project has been built successfully.
+# The script assumes that the project has been built in the build directory and 
+# the python script for plotting is located in the project_directory/Eigensolver/plot.
+# -----------------------------------------------------------------------------------------
+
 #!/bin/bash
-rm -f *.dat *.tex 
-cd ../build/ && make -j8 && cd -
-#../build/src/examples/1dharmonic
-../build/src/AutomatizedEigensolver/eigensolver
-./plot_1D.py $(ls Psi_*.dat) potential.dat
-#./plot_1D_2.py $(ls Psi*.dat) potential.dat 
+
+# Navigate to the build directory
+BUILD_DIR="../build_eigensolver"  # Change this directory to your build directory of the project
+
+echo "Cleaning up old files"
+rm -f "$BUILD_DIR"/*.dat "$BUILD_DIR"/*.tex
+
+echo "Building the project"
+(cd "$BUILD_DIR" && make -j8 && cd -) || { echo "Build failed."; exit 1; }
+
+echo "Running Eigensolver..."
+(cd "$BUILD_DIR" && ./Eigensolver) || { echo "Eigensolver execution failed."; exit 1; }
+
+# Plot results
+# this is the directory to the python script that plots the results, located in the project_directory/Eigensolver/plot
+PLOT_SCRIPT="../automatized_eigensolver/Eigensolver/plot/plot_1D.py"  # plot_1D.py can be replaced with plot_1D_2.py for a different plot style 
+
+if ls "$BUILD_DIR"/Psi_*.dat 1> /dev/null 2>&1 && [ -f "$BUILD_DIR/potential.dat" ]; then
+    python3 "$PLOT_SCRIPT" "$BUILD_DIR"/Psi_*.dat "$BUILD_DIR/potential.dat" || { echo "Plotting failed."; exit 1; }
+else
+    echo "Error: Required data files not found in $BUILD_DIR."
+    exit 1
+fi
+
+echo "All steps completed successfully!"
--- run_plot_phi.sh
+++ run_plot_phi.sh
@@ -1,15 +1,42 @@
+# ----------------------------------------------------------------------------------------
+# This script is used to make .png files of the wavefunctions and the potential
+# for each energy level. 
+# With these .png files, the user can create a gif of the wavefunctions and the potential
+# with the python script togif.py.
+# ----------------------------------------------------------------------------------------
+
+
 #!/bin/bash 
 # $1 is directory containing the data
+
+# Initialize counter for PsiCollection
 counter=0
-for directory in "$1"*; do  # loop over all directories
-    energylevel=$(echo $directory | sed -r 's/.*_([0-9]*).*/\1/') # extract energy level from directory name
-    for i in $(ls "$directory"/phi-*.dat); do # loop over all iterations of the wavefunction
-        echo $i 
-        ./plot_phi.py $(ls "$directory"/../../*Potential.dat) ${PsiCollection[@]} $i  # first argument is potential, second is previous wavefunction, third is current wavefunction
+declare -a PsiCollection
+
+# Iterate over all directories in the root folder
+for directory in "$1"*; do
+    # Extract energy level from the directory name (e.g., *_5 -> 5)
+    energylevel=$(echo $directory | sed -r 's/.*_([0-9]*).*/\1/') 
+    echo "Processing directory: $directory (Energy level: $energylevel)"
+
+    # Loop over all phi-*.dat files in the current directory
+    for wavefunction_file in "$directory"/phi-*.dat; do
+        echo "Processing wavefunction file: $wavefunction_file"
+        # Plot current wavefunction
+        # first argument is potential, second is previous wavefunction, third is current wavefunction
+        ./plot_phi.py $(ls "$directory"/../../*Potential.dat) ${PsiCollection[@]} $wavefunction_file
     done
-    PsiCollection[$counter]="$(ls "$directory"/../../*Psi/Psi_"$energylevel".dat)"  # update list of wavefunctions
+
+    # Update the PsiCollection with the Psi file corresponding to the current energy level
+    PsiCollection[$counter]="$(ls "$directory"/../../*_Psi/Psi_"$energylevel".dat)"
+
+    # Plot the final wavefunction for the current Phi directory
     ./plot_phi.py "$(ls "$directory"/../../*Potential.dat)" "${PsiCollection[@]}"
+
+    # Move the generated Psi plot to the current directory and rename it
     mv "$(ls "$directory"/../../*Psi/Psi_"$energylevel".png)" "$directory"/psi_"$energylevel".png
-    counter=$((counter+1))
+
+    # Increment the counter for PsiCollection
+    counter=$((counter + 1))
 done
 
